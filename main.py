2fa27b5b2a07e720b5519b7ccf021e4def525fdf
AntiCaffe <dongjun6288@gmail.com>
2022-07-21 16:04:44 +0900

    선인식 완료

--- main.py
+++ main.py
@@ -27,6 +27,18 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img,mask)
     return masked_image
 
+def draw_lines(img, lines, color=[255,0,0], thickness = 5):
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(img, (x1,y1),(x2,y2),color,thickness)
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    lines =     cv2.HoughLinesP(img, rho, theta, threshold,np.array([]),
+                                minLineLength = min_line_len,
+                                maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img,lines)
+    return line_img
 
 img = mpimg.imread('1.jpg')
 
@@ -65,4 +77,14 @@ plt.figure(figsize=(10,8))
 plt.imshow(mask, cmap='gray')
 plt.show()
 
+rho = 3
+theta = np.pi/180
+threshold = 10
+min_line_len = 100
+max_line_gap = 50
 
+lines = hough_lines(mask,rho,theta,threshold,min_line_len,max_line_gap)
+
+plt.figure(figsize=(10,8))
+plt.imshow(lines, cmap='gray')
+plt.show()
